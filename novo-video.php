c6ef1fd1c3d127bb2c2f97bc74ac955bdd4d3a0b
yuri leal <yurileal111@outlook.com>
2024-03-07 21:28:30 -0300

    first commit

---
+++ novo-video.php
@@ -0,0 +1,15 @@
+<?php 
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$sql = 'INSERT INTO videos (url, title) VALUES (?, ?)';
+$statement = $pdo->prepare($sql);
+$statement->bindValue (1, @$_POST['url']);
+$statement->bindValue (2, @$_POST['titulo']);
+
+if ($statement->execute() === false) {
+    header('Location: /index.php?sucesso=0');
+} else {
+    header('Location: /index.php?sucesso=1');
+}
